5e12b1d8d3c1ca67aa3fb77cdfd70970e589b93b
Kaspar Kallas <kaspar@digitalsputnik.com>
2018-06-27 15:57:55 +0300

    1st draft of manuf

--- dsaccounting/accounting/doctype/item/item.py
+++ dsaccounting/accounting/doctype/item/item.py
@@ -7,4 +7,17 @@ import frappe
 from frappe.model.document import Document
 
 class Item(Document):
-	pass
+	def autoname(self):
+		self.name = "["+self.code+"] "+self.item
+
+	def addSource(self,source):
+		#if source is marked "1" in the tabe it means OEE
+		if source == "1":
+			source = "OEE"
+		#find is source is in sources list, ignore if allready exists
+		for src in self.sources:
+			if source in src.source:
+				return
+		#add source
+		new_line = self.append("sources",{"source":source})
+		new_line.save()
