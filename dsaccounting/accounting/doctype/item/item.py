e17f3b2a12aae1103bdaac6ef9990808dc967fec
Kaspar Kallas <kaspar@digitalsputnik.com>
2018-07-05 14:01:45 +0300

    Item rename v0.1

--- dsaccounting/accounting/doctype/item/item.py
+++ dsaccounting/accounting/doctype/item/item.py
@@ -4,14 +4,25 @@
 
 from __future__ import unicode_literals
 import frappe
+import re
 from frappe.model.document import Document
 
 class Item(Document):
 	def autoname(self):
 		self.name = "["+self.code+"] "+self.item
 
+	def on_update(self):
+		#if self name [*] =! code rename the document
+		#find if the name has serial nr in the square brq
+		oldName = re.search("\[(.*)\]",self.name)
+
+		if oldName:
+			if oldName.group(1) != self.code:
+				frappe.rename_doc(self.doctype,self.name,"["+self.code+"] "+self.item)
+
+
 	def addSource(self,source):
-		#if source is marked "1" in the tabe it means OEE
+		#FIX THIS if source is marked "1" in the tabe it means OEE
 		if source == "1":
 			source = "OEE"
 		#find is source is in sources list, ignore if allready exists
